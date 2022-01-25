e66fe6c6d26e3f16d70966a9a4159a60f565e4e7
mdafsha9 <mdafsha9@gmail.com>
2022-01-24 22:19:58 -0600

    added function to header

--- firstApp/src/app/header/header.component.ts
+++ firstApp/src/app/header/header.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+      getName(name:string){
+        alert(name);
+      }
   constructor() { }
 
   ngOnInit(): void {
