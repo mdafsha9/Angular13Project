c2f6c2779d5dd6567d97f4c7faa229d8701d680e
mdafsha9 <mdafsha9@gmail.com>
2022-01-26 01:14:53 -0600

    Get input field value

--- firstApp/src/app/main-content/input-values/input-values.component.ts
+++ firstApp/src/app/main-content/input-values/input-values.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-values.component.scss']
 })
 export class InputValuesComponent implements OnInit {
-
+  displayValue:string = " ";
+  getValue(val:string){
+    console.log(val);
+    this.displayValue = val;
+  }
   constructor() { }
 
   ngOnInit(): void {
