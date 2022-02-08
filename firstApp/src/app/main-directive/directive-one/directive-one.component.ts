ace07de697db8ff21782962cbf322c5d5f78ea17
mdafsha9 <mdafsha9@gmail.com>
2022-02-08 07:24:30 -0600

    ngIf directive

--- firstApp/src/app/main-directive/directive-one/directive-one.component.ts
+++ firstApp/src/app/main-directive/directive-one/directive-one.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directive-one.component.scss']
 })
 export class DirectiveOneComponent implements OnInit {
+  goodMessage = true;
   message = false;
+  morningMessage = true;
   constructor() { }
 
   ngOnInit(): void {
