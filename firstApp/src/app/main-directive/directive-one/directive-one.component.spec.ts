7798ccf86c754ea14db9fd02a024b61445cf435e
mdafsha9 <mdafsha9@gmail.com>
2022-02-08 06:41:13 -0600

    created main-direction module and directiive-one component

---
+++ firstApp/src/app/main-directive/directive-one/directive-one.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DirectiveOneComponent } from './directive-one.component';
+
+describe('DirectiveOneComponent', () => {
+  let component: DirectiveOneComponent;
+  let fixture: ComponentFixture<DirectiveOneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DirectiveOneComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DirectiveOneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ firstApp/src/app/main-directive/directive-one/directive-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-one',
+  templateUrl: './directive-one.component.html',
+  styleUrls: ['./directive-one.component.scss']
+})
+export class DirectiveOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
