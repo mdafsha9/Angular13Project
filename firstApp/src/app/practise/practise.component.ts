32f21c718d69f4e9381d188f0a2578bf7990881d
mdafsha9 <mdafsha9@gmail.com>
2022-02-14 01:22:38 -0600

    style binding--dynamically

--- firstApp/src/app/practise/practise.component.ts
+++ firstApp/src/app/practise/practise.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practise.component.scss']
 })
 export class PractiseComponent implements OnInit {
-
+    stylePro1 = "brown";
   constructor() { }
 
   ngOnInit(): void {
