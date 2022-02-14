f769b2ee8a5f548bbbf326d931bf9c622e107c66
mdafsha9 <mdafsha9@gmail.com>
2022-02-14 01:31:25 -0600

    style binding with condition based

--- firstApp/src/app/practise/practise.component.ts
+++ firstApp/src/app/practise/practise.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practise.component.scss']
 })
 export class PractiseComponent implements OnInit {
-    stylePro1 = "brown";
+    stylePro1 = "blue";
   constructor() { }
 
   ngOnInit(): void {
