3436a1b0730ab1e6d82911a6899fc6b85bbb2136
mdafsha9 <mdafsha9@gmail.com>
2022-02-15 04:47:19 -0600

    built-in pipes

--- firstApp/src/app/my-pipes/my-pipes.component.ts
+++ firstApp/src/app/my-pipes/my-pipes.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-pipes.component.scss']
 })
 export class MyPipesComponent implements OnInit {
-
+  lowercaseExample = "ANGULAR FRAMEWORK";
+  uppercaseExample = "Learning angular";
+  dateExample = Date.now();
+  currencyExample = 125;
+  percentExample = 0.1256;
+  jsonExample = {username:'Bru', age:12, major:'IT'};
   constructor() { }
 
   ngOnInit(): void {
